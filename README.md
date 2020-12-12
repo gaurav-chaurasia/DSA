7f99c8ed1dffee70781b9184f9e1f65f1b6629b0
Gaurav Chaurasia <gaurav.181CV155@nitk.edu.in>
2020-12-12 14:13:57 +0530

    modify README.md

--- README.md
+++ README.md
@@ -4,13 +4,31 @@
 
 * All the imp concept and notes can be found in `NOTES` folder
 
-    * algorithms
-    * mathematics
-    * string
-    * pair
-    * smart pointer
-    * iterators
-    * vectors
+    * [headers `#include<>`](NOTES/headers.md)
+    * [mathematics `#include <math.h>` and general math concepts](NOTES/mathematics.md)
+    * [bitset `#include <bitset>`](NOTES/bitset.md)
+    * [string `#include <string>`](NOTES/string.md)
+    * [deque `#include <deque>`](NOTES/deque.md)
+	* [queue `#include <queue>`](NOTES/queue.md)
+	* [list `#include <list>`](NOTES/list.md)
+	* [stack `#include <stack>`](NOTES/stack.md)
+	* [pair `#include <utility>`](NOTES/utility.md)
+	* [vectors `#include <vector>`](NOTES/vector.md)
+	* [iterators `#include <iterator>`](NOTES/iterator.md)
+    * [algorithm `#include<algorithm>`](NOTES/algorithm.md)
+	* [smart pointer `#include <memory>`](NOTES/smart_pointer.md)
+
+    * [map & multimap](NOTES/map.md)
+    	* [ordered `#include <map>`](NOTES/map.md)
+    	* [unordered `#include <unordered_map>`](NOTES/map.md)
+    
+    * [set & multiset](NOTES/set.md)
+	    * [ordered `#include <set>`](NOTES/set.md)
+	    * [unordered `#include <unordered_set>`](NOTES/set.md)
+
+    * [bit](NOTES/bit.md)
+    * [bitmasking](NOTES/bitmasking.md)
+    * [limits `#include <limits>`](NOTES/limit.md)
 
 * Many imp Algorithms and their implimentation can be found in `ALGORITHM` folder
     
---
+++ my.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+#define setbits(x)      __builtin_popcountll(x) // gives number of set bits in a number
+#define zrobits(x)      __builtin_ctzll(x) // gives number of leading zeros 
+#define inf             1e18
+#define ps(x,y)         fixed<<setprecision(y)<<x // set y precision after decimal 
+#define mk(arr,n,type)  type *arr=new type[n]; // makes arr of size n 
+#define EPS   			1e-9 // almost zero 
+#define PI    			3.1415926535897932384626433832795
+#define w(x)            int x; cin>>x; while(x--)
+#define mod             1000000007 // 1e9+7
+#define deb(x)      	cout << #x << "=>" << x << endl;// print variable and it's value x=>5
+ 
+typedef long long ll;
+typedef unsigned long long ull;
+
+void c_p_c() {
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+}
+ 
+void solve() {
+    int n, k;
+    cin >> n >> k;
+    int x = ceil((-1 + pow((1+8*k), 0.5)) / 2 );
+    int firstIndex = n - x - 1;
+    int secondIndex = n - k + (((x-1)*x)/2);
+    for (int i = 0; i < n; i ++) {
+        if (i == firstIndex || i == secondIndex) {
+            cout << 'b';
+        } else {
+            cout << 'a';
+        }
+    } 
+    cout << endl;
+}
+ 
+int main() {
+	// c_p_c();
+    w(t){
+       solve();
+    }   
+    return 0;
+}

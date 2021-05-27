a971271b0b5571be903b24eb6390ebb14e56174c
Gaurav Chaurasia <gaurav.181CV155@nitk.edu.in>
2021-05-27 13:28:35 +0530

    re-structure sliding window dir

---
+++ topics/sliding_window/variable/largest_subarray_of_sum_k.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using pi = pair<int, int>;
+
+#define DEB(x) cout << "[ " << #x << " ==> " << x << " ]";
+#define w(x) int x; cin>>x; while(x--)
+#define endl '\n'
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+#define f first
+#define s second
+#define MAX_CHAR 256
+#define MOD (int)1e9+7
+
+class Solution {
+
+public:
+    void largest_subarray_of_sum_k(int n, vector<int> arr, int k) {
+        
+    }
+
+};
+
+ 
+int main() {
+    Solution* SOL = new Solution();
+    
+    SOL->SOL_01();
+    
+    delete SOL;
+    return 0;
+}

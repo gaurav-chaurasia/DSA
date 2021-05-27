7c866eed2bc7c0f3330d47ba9dc663fd9f34c3f8
Gaurav Chaurasia <gaurav.181CV155@nitk.edu.in>
2021-05-27 12:50:47 +0530

    re-structure dp dir

---
+++ topics/dp/MCM/matrix_chain_multiplication.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using pi = pair<int, int>;
+
+#define DEB(x) cout << "[ " << #x << " ==> " << x << " ]";
+#define w(x) int x; cin>>x; while(x--)
+#define endl '\n'
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+#define f first
+#define s second
+#define MAX_CHAR 256
+#define MOD (int)1e9+7
+
+class Solution {
+
+public:
+    void matrix_chain_multiplication() {
+
+    }
+
+};
+
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    Solution* SOL = new Solution();
+    
+    SOL->SOL_01();
+    
+    delete SOL;
+    return 0;
+}

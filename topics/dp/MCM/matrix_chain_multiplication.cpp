3899adb44bfa5d720bd91831bace173a53a9431f
Gaurav Chaurasia <gaurav.181CV155@nitk.edu.in>
2021-05-29 19:26:08 +0530

    dp [MCM]

--- topics/dp/MCM/matrix_chain_multiplication.cpp
+++ topics/dp/MCM/matrix_chain_multiplication.cpp
@@ -16,19 +16,52 @@ using pi = pair<int, int>;
 class Solution {
 
 public:
-    void matrix_chain_multiplication() {
+    int matrix_chain_multiplication(vector<int> arr, int i, int j) {
+        if (i >= j)
+            return 0;
+        
+        int ans = INT_MAX;
+        for (int k = i; k < j; k++) {
+            int temp =  matrix_chain_multiplication(arr, i, k) + 
+                        matrix_chain_multiplication(arr, k+1, j) + 
+                        arr[i - 1] * arr[k] * arr[j];
+            ans = min(ans, temp);
+        }
 
+        return ans;
     }
 
+    // memo
+    int matrix_chain_multiplication_memo(vector<int> arr, int i, int j, vector<vector<int>> &dp) {
+        if (i >= j)
+            return 0;
+        if (dp[i][j] != -1)
+            return dp[i][j];
+
+        int ans = INT_MAX;
+        for (int k = i; k < j; k++) {
+
+            int temp =  matrix_chain_multiplication(arr, i, k) +
+                        matrix_chain_multiplication(arr, k + 1, j) +
+                        arr[i - 1] * arr[k] * arr[j];
+
+            ans = min(ans, temp);
+        }
+
+        return dp[i][j] = ans;
+    }
 };
 
  
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(0);
     Solution* SOL = new Solution();
     
-    SOL->SOL_01();
+    vector<int> arr = {40, 20, 30, 10, 30};
+    DEB(SOL->matrix_chain_multiplication(arr, 1, 4));
+
+    int n = arr.size();
+    vector<vector<int>> dp(n + 1, vector<int>(n + 1, -1));
+    DEB(SOL->matrix_chain_multiplication_td(arr, 1, 4, dp));
     
     delete SOL;
     return 0;

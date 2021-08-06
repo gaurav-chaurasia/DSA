baaf81d8f0e808402aa6c77d41d4f8f5e0e66eaa
Gaurav Chaurasia <gaurav.181cv155@nitk.edu.in>
2021-08-06 10:17:55 +0530

    Practice [tree][graph]

--- topics/graph/bfs.cpp
+++ topics/graph/bfs.cpp
@@ -18,6 +18,30 @@ void bfs(int curr, vector<int> adj[], vector<bool> &vis, vector<int> &ans) {
     }
 }
 
+void bfs_better_implimentation(int curr, vector<int> adj[], vector<bool> &vis, vector<int> &ans) {
+    queue<int> q;
+
+    vis[curr] = true;
+    q.push(curr);
+    
+    while(!q.empty()) {
+        // this way we travel level by level
+        int size = q.size();
+        
+        while (size--) {
+            int item = q.front(); q.pop();
+            ans.push_back(item);
+
+            for (int u: adj[item]) {
+                if (vis[u] == false) {
+                    vis[u] = true;
+                    q.push(u);
+                }
+            }
+        }
+    }
+}
+
 vector<int> bfs_of_graph(int V, vector<int> adj[]) {
     vector<int> ans;
     vector<bool> vis(V, false);
